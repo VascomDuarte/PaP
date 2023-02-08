7b750603f0a6875212fc1785459e20f707c811e6
VascomDuarte <100935953+VascomDuarte@users.noreply.github.com>
2023-02-08 23:00:41 +0000

    11PM 8/2/2023
    Atualização website, fix de código, saves

--- Main/login_form.php
+++ Main/login_form.php
@@ -1,46 +1,65 @@
 <?php
 
+// Include the config_servidor.php file
 include 'config_servidor.php';
 
+// Start a new session
 session_start();
 
+// Check if the form has been submitted
 if (isset($_POST['submit'])) {
 
+   // Escape the email input and store it in the $email variable
    $email = mysqli_real_escape_string($conn, $_POST['usermail']);
-   $pass = md5($_POST['password']);
-   
 
+   // Hash the password input and store it in the $pass variable
+   $pass = md5($_POST['password']);
+
+   // Select the user with the email that was inputted
    $sql  = " SELECT * FROM user WHERE email = '$email'";
 
+   // Execute the query
    $result = mysqli_query($conn, $sql);
 
+   // Check if the query execution was successful
    if ($result === false) {
+      // If not, store an error message in the $error array
       $error[] = 'a password ou o email está incorreta';
    }
 
+   // Check if there are any rows in the result set
    $count = mysqli_num_rows($result);
    if ($count === 0) {
+     // If not, store an error message in the $error array
      $error[] = 'a password ou o email está incorreta';
    }
 
+   // Fetch the row from the result set
    $row = mysqli_fetch_assoc( $result );
 
+   // Store the password hash from the row in the $passwordHash variable
    $passwordHash = $row['password'];
+
+   // Verify the hashed password with the password input
    $passwordCheck = password_verify( $pass, $passwordHash );
-   if( ! $passwordCheck )
-   {
+
+   // Check if the password verification was successful
+   if( ! $passwordCheck ) {
+      // If not, store an error message in the $error array
       $error[] = 'a password ou o email está incorreta';
-   }else
-   {
+   } else {
+      // If the verification was successful, store the email and id in the session
       $_SESSION['usermail'] = $email;
       $_SESSION['id_user'] = $row['id'];
       $_SESSION['nome_user'] = $row['username'];
+
+      // Redirect to the fff.html page
       header('location:fff.html');
    }
-
 }
 
 ?>
+
 
 
 <!DOCTYPE html>
@@ -51,29 +70,39 @@ if (isset($_POST['submit'])) {
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="Css/acc_style.css">
+
+   <!-- Page title -->
+   <title>Login Page</title>
 </head>
 
 <body>
-
+   <!-- Login form container -->
    <div class="form-container">
-
+      <!-- Login form -->
       <form action="" method="post">
+         <!-- Form title -->
          <h3 class="title">Login</h3>
+
+         <!-- PHP code to display any error messages -->
          <?php
-         if (isset($error)) {
-            foreach ($error as $error) {
-               echo '<span class="error-msg">' . $error . '</span>';
+            if (isset($error)) {
+               foreach ($error as $error) {
+                  echo '<span class="error-msg">' . $error . '</span>';
+               }
             }
-         }
          ?>
+
+         <!-- Email input -->
          <input type="email" name="usermail" placeholder="Introduza o seu email" class="box" required>
+         <!-- Password input -->
          <input type="password" name="password" placeholder="introduza a sua password" class="box" required>
+         <!-- Submit button -->
          <input type="submit" value="LOGIN" class="form-btn" name="submit">
+         <!-- Link to register page -->
          <p>Não possui nenhuma conta? <a href="register_form.php">Registrar-se</a></p>
       </form>
-
    </div>
-
 </body>
 
 </html>
+
--- Main/register_form.php
+++ Main/register_form.php
@@ -1,44 +1,71 @@
 <?php
 
+// Include the config_servidor.php file to get access to the database
 include 'config_servidor.php';
 
+// Start a new session
 session_start();
 
+// Check if the submit button was clicked
 if (isset($_POST['submit'])) {
 
+   // Escape and store the email from the form
    $email = mysqli_real_escape_string($conn, $_POST['usermail']);
+   
+   // Hash the password from the form
    $pass = md5($_POST['password']);
+   
+   // Hash the confirmed password from the form
    $cpass = md5($_POST['cpassword']);
+   
+   // Escape and store the username from the form
    $username = mysqli_real_escape_string($conn, $_POST['uname']);
+   
+   // Store the birthdate from the form
    $date = $_POST['date'];
+   
+   // Escape and store the gender from the form
    $sex = mysqli_real_escape_string($conn, $_POST['gender']);
+   
+   // Hash and store the password for storage in the database
    $pwd_save = password_hash($pass, PASSWORD_BCRYPT);
+   
+   // Create a new DateTime object to store the current date and time
    $today = new DateTime();
+   
+   // Calculate the difference between the current date and the birthdate
    $diff = $today->diff(new DateTime($date));
+   
+   // Store the user's age in the message variable
    $message = $diff->y;
    
-
+   // Select all records from the user table where the email or username match the input from the form
    $select = " SELECT * FROM user WHERE email = '$email' or username = '$username'";
-
    $result = mysqli_query($conn, $select);
-
+   
+   // If the user's age is less than 18, show an error message
    if ($message < 18) {
       $error[] = 'Não possui idade suficiente';
    }
+   // If the email or username already exists, show an error message
    elseif (mysqli_num_rows($result) > 0){
       $error[] = 'este utilizador/email já existe!';
    }
+   // If the email and username do not already exist
    elseif(mysqli_num_rows($result) <= 0){
+      // If the password and confirmed password do not match, show an error message
       if ($pass != $cpass) {
          $error[] = 'As palavras passes não correspondem!';
       } else {
+         // If the password and confirmed password match, insert the user's information into the database
          $insert = "INSERT INTO user(username,email,password,gender,dtn_nascimento) VALUES('$username','$email','$pwd_save','$sex','$date')";
          mysqli_query($conn, $insert);
+         // Redirect the user to the login form
          header('location:login_form.php');
       }
    }
  
-   echo password_hash("rasmuslerdorf", PASSWORD_BCRYPT, array("cost" => 10));
+
 
 }
 
@@ -51,17 +78,24 @@ if (isset($_POST['submit'])) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!-- Link to the CSS stylesheet for styling the form -->
    <link rel="stylesheet" href="Css/acc_style.css">
-   <title>Webchatify</title>
+   <!-- Title for the page -->
+   <title>Register Page</title>
+   <!-- Empty link for future use -->
    <link rel="" type= "" href="">
 </head>
 
 <body>
 
+   <!-- Form container division -->
    <div class="form-container">
 
+      <!-- Registration form -->
       <form action="" method="post">
+         <!-- Form title -->
          <h3 class="title">Registro</h3>
+         <!-- Error message handling -->
          <?php
          if (isset($error)) {
             foreach ($error as $error) {
@@ -69,6 +103,7 @@ if (isset($_POST['submit'])) {
             }
          }
          ?>
+         <!-- Input fields for username, email, password, confirm password, birthdate, and gender -->
          <input type="text" name="uname" placeholder="introduza o seu username" class="box" required>
          <input type="email" name="usermail" placeholder="introduza o seu endereço email" class="box" required>
          <input type="password" name="password" placeholder="introduza a sua password" class="box" required>
@@ -78,9 +113,9 @@ if (isset($_POST['submit'])) {
          <label for="male">Masculino</label>
          <input type="radio" name="gender" value="feminino" id="female">
          <label for="female">Feminino</label>
-
-
+         <!-- Submit button -->
          <input type="submit" value="Registrar" class="form-btn" name="submit">
+         <!-- Link to login page -->
          <p>Já possui uma conta? <a href="login_form.php">Faça login agora!</a></p>
       </form>
 
